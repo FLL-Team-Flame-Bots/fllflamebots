f89dc53c0836288ed2bd78e1f675dcda466c6073
fan <8535247+fancooling@users.noreply.github.com>
2025-09-29 21:00:24 -0700

    Updated pybricks menu. Remember last choice.

--- unearthed/unearthed_main.py
+++ unearthed/unearthed_main.py
@@ -1,15 +1,44 @@
-from pybricks.tools import hub_menu, run_task
-from unearthed_run1 import run1
-from unearthed_run2 import run2
+from pybricks.tools import hub_menu
 
-# Make a menu to choose a letter. You can also use numbers.
-selected = hub_menu(range(1,7))
+from pybricks.hubs import PrimeHub
+from pybricks.tools import hub_menu, run_task, wait
+
+hub = PrimeHub()
+
+def better_menu(*options):
+    options = list(options)
+
+    last_selection = int(hub.system.storage(99, read=1)[0])
+    if last_selection not in options:
+        last_selection = options[0]
+    
+    next_idx = (options.index(last_selection) + 1) % len(options)
+    options = options[next_idx:] + options[:next_idx]
+
+    selection = hub_menu(*tuple((chr(48 + o) for o in options)))
+    selection_num = ord(selection) - 48
+    hub.system.storage(99, write=bytes([selection_num]))
+
+    return selection_num
 
 # Based on the selection, run a program.
-match selected:
-    case "1":
-        run_task(run1)
-    case "2":
-        run_task(run2)
-    case _:
-        print('!')
+def main():
+    while True:
+        selected = better_menu(1, 2, 3, 4, 5, 6, 7)
+        print(f"Selected {selected}")
+        if selected == 1:
+            import unearthed_run1
+        elif selected == 2:
+            import unearthed_run2
+        elif selected == 3:
+            hub.display.text('THREE')
+        elif selected == 4:
+            import unearthed_run4
+        elif selected == 5:
+            import unearthed_run5
+        elif selected == 6:
+            hub.display.text('SIX')
+        else:
+            break;
+
+main()
