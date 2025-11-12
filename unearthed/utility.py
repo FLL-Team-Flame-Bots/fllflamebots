1172a681e07485a5f9f823041d4a3552e3389f8f
fan <8535247+fancooling@users.noreply.github.com>
2025-11-12 08:30:35 -0800

    Add utility function to align on white black lines.

--- unearthed/utility.py
+++ unearthed/utility.py
@@ -1,4 +1,5 @@
 from pybricks.parameters import Color, Stop
+from pybricks.pupdevices import ColorSensor, Motor
 from pybricks.tools import multitask, wait
 
 
@@ -44,3 +45,52 @@ async def TurnByWheel(prime_hub, drive_base, left_wheel, right_wheel, target_ang
     right_wheel.stop()
     await drive_base.turn(target_angle - prime_hub.imu.heading())    
     print(f"heading after turn {prime_hub.imu.heading()}")
+
+async def MoveUntilBlack(wheel : Motor,
+                         color_sensor : ColorSensor,
+                         black_threshold=20,                         
+                         speed=100):
+    wheel.run(speed)
+    reflection = await color_sensor.reflection()
+    while reflection > black_threshold:
+        await wait(10)
+        reflection = await color_sensor.reflection()
+    wheel.hold()
+
+async def MoveUntilWhite(wheel : Motor,
+                         color_sensor : ColorSensor,
+                         white_threshold=90,                         
+                         speed=100):
+    wheel.run(speed)
+    reflection = await color_sensor.reflection()
+    while reflection < white_threshold:
+        await wait(10)
+        reflection = await color_sensor.reflection()
+    wheel.hold()
+
+async def AlignOnColorLine(left_wheel : Motor, 
+                           right_wheel : Motor, 
+                           left_color_sensor : ColorSensor,
+                           right_color_sensor : ColorSensor, 
+                           speed=100,
+                           black_threshold=20,
+                           white_threshold=90):    
+    # Both wheels move forward until sensed black line
+    await multitask(
+        MoveUntilBlack(left_wheel, left_color_sensor, black_threshold, speed),
+        MoveUntilBlack(right_wheel, right_color_sensor, black_threshold, speed)
+    )
+    print("Aligned on black line")
+    print(f"Left reflection {await left_color_sensor.reflection()}")
+    print(f"Right reflection {await right_color_sensor.reflection()}")
+
+    # At this point both color sensors have detected black line. Move backward 
+    # until both sensors detect white surface. Thus the base lands between white 
+    # and black lines.
+    await multitask(
+        MoveUntilWhite(left_wheel, left_color_sensor, white_threshold, -speed),
+        MoveUntilWhite(right_wheel, right_color_sensor, white_threshold, -speed)
+    )
+    print("Aligned on white line")
+    print(f"Left reflection {await left_color_sensor.reflection()}")
+    print(f"Right reflection {await right_color_sensor.reflection()}")
