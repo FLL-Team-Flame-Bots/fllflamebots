cf3669fe02511c277e333c74d3d0dc308479a7a7
fan <8535247+fancooling@users.noreply.github.com>
2025-11-23 16:38:06 -0800

    Wrap robot setup and common method in UnearthedBot

---
+++ unearthed/unearthed_bot.py
@@ -0,0 +1,83 @@
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Axis, Direction, Port, Stop
+from pybricks.pupdevices import Motor
+from pybricks.robotics import DriveBase
+from pybricks.tools import StopWatch, multitask
+from utility import straight_at_speed, turn_by_wheel
+
+
+class UnearthedBot:
+    def __init__(self):
+        # Set up all devices.
+        self.prime_hub = PrimeHub(top_side=Axis.Z, front_side=Axis.X)
+        self.watch = StopWatch()
+        self.rightwheel = Motor(Port.C, Direction.CLOCKWISE)
+        self.leftwheel = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+        self.drive_base = DriveBase(self.leftwheel, self.rightwheel, 62.4, 80)
+        self.left_motor = Motor(Port.B, Direction.CLOCKWISE)
+        self.right_motor = Motor(Port.F, Direction.COUNTERCLOCKWISE)
+
+    async def reset_motor(self, motor: Motor, forward, speed):
+        await motor.run_until_stalled(forward * speed, Stop.HOLD, 50)
+        motor.reset_angle(0)
+
+    async def reset_left_motor(self, forward=1, speed=500):
+        await self.reset_motor(self.left_motor, forward, speed)
+
+    async def reset_right_motor(self, forward=1, speed=500):
+        await self.reset_motor(self.right_motor, forward, speed)
+
+    async def reset_base(self, distance=-10):
+        await self.drive_base.straight(distance)
+        self.drive_base.reset(0, 0)
+        self.drive_base.use_gyro(True)
+
+    async def initialize(self):
+        self.watch.reset()
+        print("Battery", self.prime_hub.battery.voltage(), sep=", ")
+        await multitask(
+            self.reset_left_motor(),
+            self.reset_right_motor(),
+            self.reset_base(),
+        )
+        self.drive_base.settings(straight_speed=600)
+        self.drive_base.settings(straight_acceleration=300)
+        self.drive_base.settings(turn_rate=100)
+
+    def stop(self):
+        self.drive_base.stop()
+        print("Stopped, time (ms):", self.watch.time())
+
+    def heading(self) -> float:
+        return self.prime_hub.imu.heading()
+
+    async def turn_by_wheel(
+        self,
+        target_angle: float,
+    ):
+        await turn_by_wheel(
+            self.prime_hub,
+            self.drive_base,
+            self.leftwheel,
+            self.rightwheel,
+            target_angle,
+        )
+
+    async def straight_at_speed(
+        self,
+        distance: float,
+        speed: int = -1,
+        acceleration: int = -1,
+        then=Stop.COAST,
+    ):
+        """
+        Wrapper for the utility function of the same name.
+        Moves the robot straight for a given distance at a specified speed.
+        """
+        await straight_at_speed(
+            self.drive_base,
+            distance,
+            speed=speed,
+            acceleration=acceleration,
+            then=then,
+        )
--- unearthed/unearthed_v3_mineshaft.py
+++ unearthed/unearthed_v3_mineshaft.py
@@ -1,79 +1,49 @@
-from pybricks.hubs import PrimeHub
-from pybricks.parameters import Axis, Direction, Port, Stop
+from pybricks.parameters import Stop
 from pybricks.pupdevices import Motor
-from pybricks.robotics import DriveBase
-from pybricks.tools import StopWatch, multitask, run_task, wait
+from pybricks.tools import multitask, run_task, wait
+from unearthed_bot import UnearthedBot
 
-from utility import turn_by_wheel, straight_at_speed
-
-# Set up all devices.
-prime_hub = PrimeHub(top_side=Axis.Z, front_side=Axis.X)
-watch = StopWatch()
-rightwheel = Motor(Port.C, Direction.CLOCKWISE)
-leftwheel = Motor(Port.D, Direction.COUNTERCLOCKWISE)
-drive_base = DriveBase(leftwheel, rightwheel, 62.4, 114)
-left_motor = Motor(Port.B, Direction.CLOCKWISE)
-right_motor = Motor(Port.F, Direction.COUNTERCLOCKWISE)
-
-
-async def reset_left_motor():
-    await left_motor.run_until_stalled(500, Stop.HOLD, 50)
-    left_motor.reset_angle(0)
-
-
-async def reset_right_motor():
-    await right_motor.run_until_stalled(500, Stop.HOLD, 50)
-    right_motor.reset_angle(0)
-
-
-async def reset_base():
-    await drive_base.straight(-10)
-    drive_base.reset(0, 0)
-    drive_base.use_gyro(True)
+bot = UnearthedBot()
 
 
 async def main():
-    watch.reset()
-    print("Battery", prime_hub.battery.voltage(), sep=", ")
-    await multitask(
-        reset_left_motor(),
-        reset_right_motor(),
-        reset_base(),
-    )
-    drive_base.settings(straight_speed=600)
-    drive_base.settings(straight_acceleration=300)
-    drive_base.settings(turn_rate=100)
+    # Set up all devices.
+    bot.initialize()
+    prime_hub = bot.prime_hub
+    drive_base = bot.drive_base
+    left_motor = bot.left_motor
+    right_motor = bot.right_motor
+
     await drive_base.straight(710)
-    await turn_by_wheel(prime_hub, drive_base, leftwheel, rightwheel, 90)
-    await straight_at_speed(drive_base, 110, speed=300, acceleration=200)
+    await bot.turn_by_wheel(90)
+    await bot.straight_at_speed(110, speed=300, acceleration=200)
     # drop flag
     await right_motor.run_angle(300, -200)
-    await straight_at_speed(drive_base, 185, speed=300, acceleration=200)
-    
+    await bot.straight_at_speed(185, speed=300, acceleration=200)
+
     # Face mission 4, back up a bit, drop right arm all the way down.
-    await turn_by_wheel(prime_hub, drive_base, leftwheel, rightwheel, 0)
+    await bot.turn_by_wheel(0)
     await drive_base.straight(-135)
     await right_motor.run_until_stalled(-300, Stop.HOLD, 50)
-    print(f"heading after backoff {prime_hub.imu.heading()}")
+    print(f"heading after backoff {bot.heading()}")
     print(f"right motor angle after stalled {right_motor.angle()}")
     # Move toward mission 4, raise right arm to lift mineshaft, then
     # move and continue lifting mineshaft.
     await drive_base.straight(80)
-    await turn_by_wheel(prime_hub, drive_base, leftwheel, rightwheel, 0)
-    print(f"heading toward mission 4 {prime_hub.imu.heading()}")
+    await bot.turn_by_wheel(0)  # fine tune as move back&forth would veer off
+    print(f"heading toward mission 4 {bot.heading()}")
     await right_motor.run_target(300, -370)
     await multitask(
-        straight_at_speed(drive_base, 115, speed=140, acceleration=100),
+        bot.straight_at_speed(115, speed=140, acceleration=100),
         right_motor.run_target(200, -320),
     )
-    await turn_by_wheel(prime_hub, drive_base, leftwheel, rightwheel, 0)
-    #await wait(100)
+    await bot.turn_by_wheel(0)
+    # await wait(100)
 
     # Lower left arm to pick up artifact.
     await multitask(
         wait(2000),
         left_motor.run_until_stalled(-500, Stop.HOLD, 80),
-        # left_motor.run_angle(1000, -290),
         race=True,
     )
     await left_motor.run_angle(150, 270)
@@ -86,12 +56,11 @@ async def main():
     await left_motor.run_until_stalled(1000, Stop.HOLD, 50)
     # await left_motor.run_angle(1000, 100)
     await drive_base.straight(-70)
-    await drive_base.turn(prime_hub.imu.heading() + 70)
-    await turn_by_wheel(prime_hub, drive_base, leftwheel, rightwheel, -290)
-    print(f"heading toward last flag {prime_hub.imu.heading()}")
+    await bot.turn_by_wheel(70)
+    print(f"heading toward last flag {bot.heading()}")
     await drive_base.straight(460)
-    print(f"heading delivered last flag {prime_hub.imu.heading()}")
-    print("Total time (ms):", watch.time())
+    print(f"heading delivered last flag {bot.heading()}")
+    bot.stop()
 
 
 run_task(main())
