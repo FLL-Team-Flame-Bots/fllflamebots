f057e06161ad1c4c8388cde2d85a0eca8bf465f0
fan <8535247+fancooling@users.noreply.github.com>
2025-11-24 20:19:57 -0800

    Rename initialize to init_setting

--- unearthed/unearthed_bot.py
+++ unearthed/unearthed_bot.py
@@ -33,7 +33,7 @@ class UnearthedBot:
         self.drive_base.reset(0, 0)
         self.drive_base.use_gyro(True)
 
-    def initialize(self):
+    def init_setting(self):
         self.watch.reset()
         print("Battery", self.prime_hub.battery.voltage(), sep=", ")
         self.drive_base.settings(straight_speed=600)
